043df25bc6a0119400c655d9345759a8733d579a
unknown <wperez185@yahoo.com>
2020-05-16 19:00:29 -0400

    fixed name of component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import Todo from './components/Nav'
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
-     <Todo />
+    <div className="App container-fluid">
+     <Nav />
+     <ul class="list-group">
+        <li class="list-group-item active">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+     </ul>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return(
         <nav class="navbar navbar-light bg-light">
             <h3>
-              <a class="navbar-brand" href="#">
+              <a class="navbar-brand" href="/">
               <img src={skills} width="50" height="50" alt="" loading="lazy"></img> TODO
               </a>
             </h3>
